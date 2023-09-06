a405417721e22bca43128d415dc49b08267f9282
chintansakhiya <chintan.sakhiya@improwised.com>
2023-09-06 15:22:59 +0530

    fix: code bug

--- poster_downloader.go
+++ poster_downloader.go
@@ -1,13 +1,14 @@
-package poster_downloader
+// package main
+package MoviePoster
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 )
 
 type getData struct {
@@ -19,13 +20,14 @@ type getPath struct {
 	OriginalTitle string `json:"original_title"`
 	Path          string `json:"poster_path"`
 }
- 
-func Downloader(titles []string) {
 
-	for i := 0; i < len(titles); i++ {
-
-		pathString := `https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=` + titles[i]
-		res, err := http.Get(pathString)
+func Downloader(movieTitles []string) {
+	createFolder()
+	var wg sync.WaitGroup
+	for i := 0; i < len(movieTitles); i++ {
+		temp := strings.ReplaceAll(movieTitles[i], " ", "%20")
+		reqUrl := `https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=` + temp
+		res, err := http.Get(reqUrl)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -35,46 +37,71 @@ func Downloader(titles []string) {
 
 		var data getData
 		json.Unmarshal(posterPath, &data)
-
 		if data.TotalResults != 0 {
 
 			fileNamae := strings.ReplaceAll(data.Results[0].Path, "/", "")
-			err:=downloadFile(data.Results[0].Path, fileNamae)
-			if err!=nil {
-				fmt.Println(err)
-			}
+			wg.Add(1)
+			go downloadFile(data.Results[0].Path, fileNamae, &wg, movieTitles[i])
+
 		} else {
-			fmt.Println(titles[i] + " not found")
+			fmt.Println(movieTitles[i] + " not found")
 		}
-
 	}
-
-	//---------------------------------------------------------------------------
+	wg.Wait()
 }
 
-func downloadFile(URL, fileName string) error {
+func downloadFile(URL, fileName string, wg *sync.WaitGroup, movieTitles string) {
 	//Get the response bytes from the url
 	response, err := http.Get("https://image.tmdb.org/t/p/original" + URL)
 	if err != nil {
-		return err
+		wg.Done()
+		fmt.Println(err)
+		return
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != 200 {
-		return errors.New("Received non 200 response code")
+		wg.Done()
+		fmt.Println("Received non 200 response code")
+		return
 	}
 	//Create a empty file
 	file, err := os.Create("img/" + fileName)
 	if err != nil {
-		return err
+		wg.Done()
+		fmt.Println(err)
+		return
 	}
 	defer file.Close()
 
 	//Write the bytes to the fiel
 	_, err = io.Copy(file, response.Body)
 	if err != nil {
-		return err
+		wg.Done()
+		fmt.Println(err)
+		return
 	}
+	wg.Done()
+	fmt.Println("Downloaded: " + movieTitles)
+}
 
-	return nil
+func createFolder() {
+	folderPath := "./img"
+
+	// Check if the folder already exists
+	_, err := os.Stat(folderPath)
+	if os.IsNotExist(err) {
+		// The folder does not exist, so create it
+		err := os.MkdirAll(folderPath, os.ModePerm)
+		if err != nil {
+			fmt.Println("Error creating folder:", err)
+		} else {
+			fmt.Println("Folder created successfully.")
+		}
+	} else if err != nil {
+		// Handle other errors, such as permission issues
+		fmt.Println("Error checking folder:", err)
+	} else {
+		fmt.Printf("%v Folder already exists.\n", folderPath)
+	}
 }
